f70ff709d663515651489328b29c18067dc5813b
2bambitious <2bambitious@gmail.com>
2022-07-20 20:57:04 +0900

    Feat: "Add badge"
    side badge부분 추가

--- js/main.js
+++ js/main.js
@@ -19,3 +19,27 @@ searchInputEl.addEventListener('blur', function () {
   // placeholder 부분 빈 문자로
   searchInputEl.setAttribute('placeholder', '');
 })
+
+const badgeEl = document.querySelector('header .badges');
+
+// window : brower 창
+window.addEventListener('scroll', _.throttle(function () {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    // hide badge
+    //gsap.to(요소, 지속시간, 옵션)
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+
+  } else {
+    // show badge
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+    
+  }
+}, 300));
+// _.throttle(함수, 시간(ms)
